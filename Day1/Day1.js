b2bf9b307904889976d36ab45c2038d18b798a16
deepak01112002 <deepak8574757609@gmail.com>
2024-10-17 17:54:57 +0530

    1st Dsa question

---
+++ Day1/Day1.js
@@ -0,0 +1,34 @@
+// we have one array and one Target we have to Find that Target , If target present than we have to return its index else -1
+
+// let arr = [1,2,3,4,5,6];
+//  let target = 5;
+//  output = 4; 
+
+
+// let arr = [1,2,3,4,5,6];
+//  let target = 9;
+//  output = -1; 
+
+let bag = -1
+
+for(let x=0; x<arr.length; x++){
+    if(arr[x] == target){
+        bag = x;
+    }
+}
+
+console.log(bag)
+
+
+
+function solve(arr,target){
+    for(let x=0; x<arr.length; x++){
+        if(arr[x] == target){
+            return x;
+        }
+    }
+
+    return -1
+}
+
+console.log(solve())
